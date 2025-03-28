760b7fbdecc7404d5bb2127e5c089f1763b8d424
Bogdan Stefan Sandoiu B.Sandoiu@student.rug.nl <b.sandoiu@student.rug.nl>
2025-03-28 19:18:49 +0100

    the pitch player detection video is not working properly

--- ai_futbol_main.py
+++ ai_futbol_main.py
@@ -11,22 +11,37 @@ from more_itertools import chunked
 from umap.umap_ import UMAP
 from sklearn.cluster import KMeans
 from team_classifier import TeamClassifier
+from pitch_config import SoccerPitchConfiguration
+from view_transformer import ViewTransformer
+from pitch_draw import draw_pitch, draw_points_on_pitch
 
 #os.makedirs("example_matches", exist_ok=True)
 #url = "https://drive.google.com/uc?id=12TqauVZ9tLAv8kWxTTBFWtgt2hNQ4_ZF"
 #url_path = "example_matches/0bfacc_0.mp4"
+#url = "https://drive.google.com/uc?id=1vVwjW1dE1drIdd4ZSILfbCGPD4weoNiu"
+#url_path = "/home2/s5549329/ml-futbol/example_matches/121364_0.mp4"
 #gdown.download(url, url_path, quiet=False)
+#Input a number of which type of video you want 
+NUMBER = 3
 
-IMAGES_FOLDER_PATH = "/home2/s5549329/ml-futbol/saved_images/anottated_video.mp4"
+TYPE_OF_VIDEO = {0: "None", 1: "Player_Detection_Video", 2: "Pitch_Detection_Video", 3: "All_Detection_Video"}
+SOURCE_VIDEO_PATH = "/home2/s5549329/ml-futbol/example_matches/121364_0.mp4"
 
+IMAGES_FOLDER_PATH_PLAYER_DETECTION = "/home2/s5549329/ml-futbol/saved_images/anottated_video.mp4"
+IMAGES_FOLDER_PATH_PITCH_DETECTION = "/home2/s5549329/ml-futbol/saved_images/anotatted_pitch_video.mp4"
+IMAGES_FOLDER_PATH_PITCH_PLAYER_DETECTION = "/home2/s5549329/ml-futbol/saved_images/anotatted_pitch_player_video.mp4"
+IMAGES_FOLDER_PATH = "/home2/s5549329/ml-futbol/saved_images"
+
+FIELD_DTECTION_MODEL = YOLO("/home2/s5549329/ml-futbol/models/best_pitch.pt")
 PLAYER_DETECTION_MODEL = YOLO("/home2/s5549329/ml-futbol/models/best.pt")
 
-SOURCE_VIDEO_PATH = "/home2/s5549329/ml-futbol/example_matches/0bfacc_0.mp4"
+CONFIG = SoccerPitchConfiguration()
+
 print(os.path.exists(SOURCE_VIDEO_PATH))
 
 # saving the first frame of the video
-frame_generator = sv.get_video_frames_generator(SOURCE_VIDEO_PATH)
-frame = next(frame_generator)
+#frame_generator = sv.get_video_frames_generator(SOURCE_VIDEO_PATH)
+#frame = next(frame_generator)
 
 #frame_path = os.path.join(IMAGES_FOLDER_PATH, f"frame_{0:03d}.jpg")
 #cv2.imwrite(frame_path, frame)
@@ -38,6 +53,7 @@ PLAYER_ID = 2
 REFEREE_ID = 3
 STRIDE = 30
 
+#stride used just for dividing the teams
 frame_generator = sv.get_video_frames_generator(SOURCE_VIDEO_PATH, stride=STRIDE)
 
 crops = []
@@ -56,10 +72,6 @@ team_classifier.fit(crops)
 #crops_path = os.path.join(IMAGES_FOLDER_PATH, f"crops_{100:03d}.jpg")
 #cv2.imwrite(crops_path, crops_image)
 
-
-#Dividing the players into two systems by using Kmeans clustering
-
-
 #Resolve assigning goalkeepers to the right team
 def resolve_goalkeepers_team_id(players: sv.Detections, goalkeepers: sv.Detections) -> np.ndarray:
     goalkeepers_xy = goalkeepers.get_anchors_coordinates(sv.Position.BOTTOM_CENTER)
@@ -75,74 +87,279 @@ def resolve_goalkeepers_team_id(players: sv.Detections, goalkeepers: sv.Detectio
     return np.array(goalkeepers_team_id)
 
 
-# The main part where the video is being produced
-ellipse_annotator = sv.EllipseAnnotator(
-    color=sv.ColorPalette.from_hex(['#00BFFF', '#FF1493', '#FFD700']),
-    thickness=2
-)
-label_annotator = sv.LabelAnnotator(
-    color=sv.ColorPalette.from_hex(['#00BFFF', '#FF1493', '#FFD700']),
-    text_color=sv.Color.from_hex('#000000'),
-    text_position=sv.Position.BOTTOM_CENTER
-)
-triangle_annotator = sv.TriangleAnnotator(
-    color=sv.Color.from_hex('#FFD700'),
-    base=25,
-    height=21,
-    outline_thickness=1
-)
+# The main part where the player, ball, referee, goalkeeper detection is being produced
+if TYPE_OF_VIDEO[NUMBER] == "Player_Detection_Video":
+    ellipse_annotator = sv.EllipseAnnotator(
+        color=sv.ColorPalette.from_hex(['#00BFFF', '#FF1493', '#FFD700']),
+        thickness=2
+    )
+    label_annotator = sv.LabelAnnotator(
+        color=sv.ColorPalette.from_hex(['#00BFFF', '#FF1493', '#FFD700']),
+        text_color=sv.Color.from_hex('#000000'),
+        text_position=sv.Position.BOTTOM_CENTER
+    )
+    triangle_annotator = sv.TriangleAnnotator(
+        color=sv.Color.from_hex('#FFD700'),
+        base=25,
+        height=21,
+        outline_thickness=1
+    )
 
-tracker = sv.ByteTrack()
-tracker.reset()
+    tracker = sv.ByteTrack()
+    tracker.reset()
 
-video_info = sv.VideoInfo.from_video_path(SOURCE_VIDEO_PATH)
-videosnk = sv.VideoSink(IMAGES_FOLDER_PATH, video_info)
-frame_generator = sv.get_video_frames_generator(SOURCE_VIDEO_PATH)
+    video_info = sv.VideoInfo.from_video_path(SOURCE_VIDEO_PATH)
+    videosnk = sv.VideoSink(IMAGES_FOLDER_PATH_PLAYER_DETECTION, video_info)
+    frame_generator = sv.get_video_frames_generator(SOURCE_VIDEO_PATH)
 
-with videosnk:
-    for frame in tqdm(frame_generator, total=video_info.total_frames, desc="video processing"):
-        result  = PLAYER_DETECTION_MODEL.predict(frame, conf=0.3)[0]
-        detections = sv.Detections.from_ultralytics(result)
+    with videosnk:
+        for frame in tqdm(frame_generator, total=video_info.total_frames, desc="video processing PLAYER DETECTION"):
+            result  = PLAYER_DETECTION_MODEL.predict(frame, conf=0.3)[0]
+            detections = sv.Detections.from_ultralytics(result)
 
-        ball_detections = detections[detections.class_id == BALL_ID]
-        ball_detections.xyxy = sv.pad_boxes(xyxy=ball_detections.xyxy, px=10)
+            ball_detections = detections[detections.class_id == BALL_ID]
+            ball_detections.xyxy = sv.pad_boxes(xyxy=ball_detections.xyxy, px=10)
 
-        all_detections = detections[detections.class_id != BALL_ID]
-        all_detections = all_detections.with_nms(threshold=0.5, class_agnostic=True)
-        all_detections = tracker.update_with_detections(detections=all_detections)
+            all_detections = detections[detections.class_id != BALL_ID]
+            all_detections = all_detections.with_nms(threshold=0.5, class_agnostic=True)
+            all_detections = tracker.update_with_detections(detections=all_detections)
 
-        goalkeepers_detections = all_detections[all_detections.class_id == GOALKEEPER_ID]
-        players_detections = all_detections[all_detections.class_id == PLAYER_ID]
-        referees_detections = all_detections[all_detections.class_id == REFEREE_ID]
+            goalkeepers_detections = all_detections[all_detections.class_id == GOALKEEPER_ID]
+            players_detections = all_detections[all_detections.class_id == PLAYER_ID]
+            referees_detections = all_detections[all_detections.class_id == REFEREE_ID]
 
-        players_crops = [sv.crop_image(frame, xyxy) for xyxy in players_detections.xyxy]
-        players_detections.class_id = team_classifier.predict(players_crops)
+            players_crops = [sv.crop_image(frame, xyxy) for xyxy in players_detections.xyxy]
+            players_detections.class_id = team_classifier.predict(players_crops)
 
-        goalkeepers_detections.class_id = resolve_goalkeepers_team_id(players_detections, goalkeepers_detections)
+            goalkeepers_detections.class_id = resolve_goalkeepers_team_id(players_detections, goalkeepers_detections)
 
-        referees_detections.class_id -= 1
+            referees_detections.class_id -= 1
 
-        all_detections = sv.Detections.merge([
-            players_detections, goalkeepers_detections, referees_detections])
+            all_detections = sv.Detections.merge([
+                players_detections, goalkeepers_detections, referees_detections])
 
-        labels = [
-            f"#{tracker_id}"
-            for tracker_id
-            in all_detections.tracker_id
-        ]
+            labels = [
+                f"#{tracker_id}"
+                for tracker_id
+                in all_detections.tracker_id
+            ]
 
-        all_detections.class_id = all_detections.class_id.astype(int)
+            all_detections.class_id = all_detections.class_id.astype(int)
 
-        annotated_frame = frame.copy()
-        annotated_frame = ellipse_annotator.annotate(
-            scene=annotated_frame,
-            detections=all_detections)
-        annotated_frame = label_annotator.annotate(
-            scene=annotated_frame,
-            detections=all_detections,
-            labels=labels)
-        annotated_frame = triangle_annotator.annotate(
-            scene=annotated_frame,
-            detections=ball_detections)
-        videosnk.write_frame(annotated_frame)
+            annotated_frame = frame.copy()
+            annotated_frame = ellipse_annotator.annotate(
+                scene=annotated_frame,
+                detections=all_detections)
+            annotated_frame = label_annotator.annotate(
+                scene=annotated_frame,
+                detections=all_detections,
+                labels=labels)
+            annotated_frame = triangle_annotator.annotate(
+                scene=annotated_frame,
+                detections=ball_detections)
+            videosnk.write_frame(annotated_frame)
+
+#The part where the pitch is being detected
+if TYPE_OF_VIDEO[NUMBER] == "Pitch_Detection_Video": 
+
+    edge_annotator = sv.EdgeAnnotator(
+        color=sv.Color.from_hex('#00BFFF'),
+        thickness=2, edges=CONFIG.edges)
+    vertex_annotator = sv.VertexAnnotator(
+        color=sv.Color.from_hex('#FF1493'),
+        radius=8)
+    vertex_annotator_2 = sv.VertexAnnotator(
+        color=sv.Color.from_hex('#00BFFF'),
+        radius=8)
+
+    frame_generator = sv.get_video_frames_generator(SOURCE_VIDEO_PATH)
+
+    video_info = sv.VideoInfo.from_video_path(SOURCE_VIDEO_PATH)
+    videosnk = sv.VideoSink(IMAGES_FOLDER_PATH_PITCH_DETECTION, video_info)
+    frame_generator = sv.get_video_frames_generator(SOURCE_VIDEO_PATH)
+
+    with videosnk:
+        for frame in tqdm(frame_generator, total=video_info.total_frames, desc="video processing"):
+            result = FIELD_DTECTION_MODEL.predict(frame, conf=0.3)[0]
+            key_points = sv.KeyPoints.from_ultralytics(result)
+            print(key_points.confidence)
+
+            filter = key_points.confidence[0] > 0.5
+            frame_reference_points = key_points.xy[0][filter]
+            frame_reference_key_points = sv.KeyPoints(
+                xy=frame_reference_points[np.newaxis, ...])
+
+            pitch_reference_points = np.array(CONFIG.vertices)[filter]
+
+            transformer = ViewTransformer(
+                source=pitch_reference_points,
+                target=frame_reference_points
+            )
+
+            pitch_all_points = np.array(CONFIG.vertices)
+            frame_all_points = transformer.transform_points(points=pitch_all_points)
+
+            frame_all_key_points = sv.KeyPoints(xy=frame_all_points[np.newaxis, ...])
+
+            annotated_frame = frame.copy()
+            annotated_frame = edge_annotator.annotate(
+                scene=annotated_frame,
+                key_points=frame_all_key_points)
+            annotated_frame = vertex_annotator_2.annotate(
+                scene=annotated_frame,
+                key_points=frame_all_key_points)
+            annotated_frame = vertex_annotator.annotate(
+                scene=annotated_frame,
+                key_points=frame_reference_key_points)
+            videosnk.write_frame(annotated_frame)
+
+#combining both player and pitch detection
+elif TYPE_OF_VIDEO[NUMBER] == "All_Detection_Video":
+    tracker = sv.ByteTrack()
+    tracker.reset()
+
+    frame_generator = sv.get_video_frames_generator(SOURCE_VIDEO_PATH)
+    video_info = sv.VideoInfo.from_video_path(SOURCE_VIDEO_PATH)
+    videosnk = sv.VideoSink(IMAGES_FOLDER_PATH_PITCH_PLAYER_DETECTION, video_info)
+    frame_generator = sv.get_video_frames_generator(SOURCE_VIDEO_PATH)
+
+    with videosnk:
+        for frame in tqdm(frame_generator, total=video_info.total_frames, desc="Processing video"):
+            result = PLAYER_DETECTION_MODEL.predict(frame, conf=0.3)[0]
+            detections = sv.Detections.from_ultralytics(result)
+
+            ball_detections = detections[detections.class_id == BALL_ID]
+            ball_detections.xyxy = sv.pad_boxes(xyxy=ball_detections.xyxy, px=10)
+
+            all_detections = detections[detections.class_id != BALL_ID]
+            all_detections = all_detections.with_nms(threshold=0.5, class_agnostic=True)
+            all_detections = tracker.update_with_detections(detections=all_detections)
+
+            goalkeepers_detections = all_detections[all_detections.class_id == GOALKEEPER_ID]
+            players_detections = all_detections[all_detections.class_id == PLAYER_ID]
+            referees_detections = all_detections[all_detections.class_id == REFEREE_ID]
+
+            players_crops = [sv.crop_image(frame, xyxy) for xyxy in players_detections.xyxy]
+            if players_crops:  # only predict if there are crops
+                players_detections.class_id = team_classifier.predict(players_crops)
+            try:
+                goalkeepers_detections.class_id = resolve_goalkeepers_team_id(players_detections, goalkeepers_detections)
+            except Exception as e:
+                print("Error resolving goalkeeper team IDs:", e)
+            referees_detections.class_id -= 1
+
+            merged_detections = sv.Detections.merge([
+                players_detections, goalkeepers_detections, referees_detections
+            ])
+
+            labels = [f"#{tid}" for tid in merged_detections.tracker_id]
+            merged_detections.class_id = merged_detections.class_id.astype(int)
+            
+            # Use the field detection model to get pitch keypoints
+            field_result = FIELD_DTECTION_MODEL.predict(frame, conf=0.3)[0]
+            key_points = sv.KeyPoints.from_ultralytics(field_result)
+            print(key_points.confidence)
+
+            filter_mask = key_points.confidence[0] > 0.5
+            frame_reference_points = key_points.xy[0][filter_mask]
+            pitch_reference_points = np.array(CONFIG.vertices)[filter_mask]
+
+            transformer = ViewTransformer(
+                source=frame_reference_points,
+                target=pitch_reference_points
+            )
+
+            frame_ball_xy = ball_detections.get_anchors_coordinates(sv.Position.BOTTOM_CENTER)
+            pitch_ball_xy = transformer.transform_points(points=frame_ball_xy)
+
+            players_xy = players_detections.get_anchors_coordinates(sv.Position.BOTTOM_CENTER)
+            pitch_players_xy = transformer.transform_points(points=players_xy)
+
+            referees_xy = referees_detections.get_anchors_coordinates(sv.Position.BOTTOM_CENTER)
+            pitch_referees_xy = transformer.transform_points(points=referees_xy)
+
+            annotated_frame = draw_pitch(CONFIG)
+            annotated_frame = draw_points_on_pitch(
+                config=CONFIG,
+                xy=pitch_ball_xy,
+                face_color=sv.Color.WHITE,
+                edge_color=sv.Color.BLACK,
+                radius=10,
+                pitch=annotated_frame
+            )
+            annotated_frame = draw_points_on_pitch(
+                config=CONFIG,
+                xy=pitch_players_xy[players_detections.class_id == 0],
+                face_color=sv.Color.from_hex('00BFFF'),
+                edge_color=sv.Color.BLACK,
+                radius=16,
+                pitch=annotated_frame
+            )
+            annotated_frame = draw_points_on_pitch(
+                config=CONFIG,
+                xy=pitch_players_xy[players_detections.class_id == 1],
+                face_color=sv.Color.from_hex('FF1493'),
+                edge_color=sv.Color.BLACK,
+                radius=16,
+                pitch=annotated_frame
+            )
+            annotated_frame = draw_points_on_pitch(
+                config=CONFIG,
+                xy=pitch_referees_xy,
+                face_color=sv.Color.from_hex('FFD700'),
+                edge_color=sv.Color.BLACK,
+                radius=16,
+                pitch=annotated_frame
+            )
+
+            videosnk.write_frame(annotated_frame)
+elif NUMBER == 0:
+    edge_annotator = sv.EdgeAnnotator(
+        color=sv.Color.from_hex('#00BFFF'),
+        thickness=2, edges=CONFIG.edges)
+    vertex_annotator = sv.VertexAnnotator(
+        color=sv.Color.from_hex('#FF1493'),
+        radius=8)
+    vertex_annotator_2 = sv.VertexAnnotator(
+        color=sv.Color.from_hex('#00BFFF'),
+        radius=8)
+
+    frame_generator = sv.get_video_frames_generator(SOURCE_VIDEO_PATH)
+    frame = next(frame_generator)
+
+    result = FIELD_DTECTION_MODEL.predict(frame, conf=0.3)[0]
+    key_points = sv.KeyPoints.from_ultralytics(result)
+
+    filter = key_points.confidence[0] > 0.5
+    frame_reference_points = key_points.xy[0][filter]
+    frame_reference_key_points = sv.KeyPoints(
+        xy=frame_reference_points[np.newaxis, ...])
+
+    pitch_reference_points = np.array(CONFIG.vertices)[filter]
+
+    transformer = ViewTransformer(
+        source=pitch_reference_points,
+        target=frame_reference_points
+    )
+
+    pitch_all_points = np.array(CONFIG.vertices)
+    frame_all_points = transformer.transform_points(points=pitch_all_points)
+
+    frame_all_key_points = sv.KeyPoints(xy=frame_all_points[np.newaxis, ...])
+
+    annotated_frame = frame.copy()
+    annotated_frame = edge_annotator.annotate(
+        scene=annotated_frame,
+        key_points=frame_all_key_points)
+    annotated_frame = vertex_annotator_2.annotate(
+        scene=annotated_frame,
+        key_points=frame_all_key_points)
+    annotated_frame = vertex_annotator.annotate(
+        scene=annotated_frame,
+        key_points=frame_reference_key_points)
+
+    frame_path = os.path.join(IMAGES_FOLDER_PATH, f"frame_{0:03d}.jpg")
+    cv2.imwrite(frame_path, annotated_frame)
+        
+
 
